bda36c5ff28449951f1fe8868bc07b58d9afc95f
MKnoles <mknoles78@gmail.com>
2020-04-16 22:28:19 -0400

    updated seeds and tested in db

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,33 +5,33 @@ VALUES ("Amazing Spider-Man", "Comics", 300, now(), now());
 
 select * from collections;
 
-INSERT INTO Cards (manufacturer, player, year, grade)
-VALUES ("Topps", "Barry Bonds", 1987, "Fair");
+INSERT INTO Cards (manufacturer, player, year, quality, createdAt, updatedAt)
+VALUES ("Topps", "Barry Bonds", 1987, "Fair", now(), now());
 
 select * from Cards;
 
-INSERT INTO Comics (publisher, title, issue, grade)
-VALUES ("Marvel", "Amazing Fantasy", 15, "Mint");
+INSERT INTO Comics (publisher, title, issue, quality, createdAt, updatedAt)
+VALUES ("Marvel", "Amazing Fantasy", 15, "Mint", now(), now());
 
 select * from Comics;
 
-INSERT INTO Records (artist, album, grade)
-VALUES ("Prince", "Purple Rain", "Mint");
+INSERT INTO Records (artist, album, quality, createdAt, updatedAt)
+VALUES ("Prince", "Purple Rain", "Mint", now(), now());
 
 select * from Records;
 
-INSERT INTO Stamps (origincountry, class, postmark, grade)
-VALUES ("United States", "Commerative", TRUE, "Fair");
+INSERT INTO Stamps (origincountry, class, postmark, quality, createdAt, updatedAt)
+VALUES ("United States", "Commerative", TRUE, "Fair", now(), now());
 
 select * from Stamps;
 
-INSERT INTO Toys (manufacturer, make, model, grade)
-VALUES ("Mattel", "Barbie", "Christmas Edition", "Mint");
+INSERT INTO Toys (manufacturer, make, model, quality, createdAt, updatedAt)
+VALUES ("Mattel", "Barbie", "Christmas Edition", "Mint", now(), now());
 
 select * from Toys;
 
-INSERT INTO Wines (vineyard, style, variety, vintage, winegrade)
-VALUES ("Mondavi", "Red", "Merlot", 1985, "Mint");
+INSERT INTO Wines (vineyard, style, variety, vintage, winegrade, createdAt, updatedAt)
+VALUES ("Mondavi", "Red", "Merlot", 1985, 90, now(), now());
 
 select * from Wines;
  
